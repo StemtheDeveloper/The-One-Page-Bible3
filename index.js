59d154596c511d1d4fcc34efbf67d13f6b3c4846
StemtheDeveloper <127262716+StemtheDeveloper@users.noreply.github.com>
2023-08-07 16:50:07 +1200

    "removed obsolete files and folders"

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-const express = require('express');
-const paypal = require('paypal-rest-sdk');
+import "dotenv/config";
+import express from "express";
+import * as paypal from "./paypal-api.js";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  const payment = paypal.Payment({
-    amount: 100,
-    currency: 'USD',
-    payer: {
-      email: 'johndoe@example.com',
-    },
-  });
-
-  payment.create((err, payment) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(payment);
-    }
-  });
+try {
+    app.use(express.static("public"));
+} catch(err) {
+  res.status(500).send(err.message);
+  }
+//get client token
+app.post("/api/token", async (req, res) => {
+  try {
+    const token= await paypal.generateClientToken();
+    res.json(token);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
-app.listen(3000);
+app.get('/success',async(req,res) => {
+if(req.query.liability_shift==true || req.query.liability_shift=='UNKNOWN');
+res.sendFile('./public/success.html', {root:'.'});
+});
+// create order
+app.post("/api/orders", async (req, res) => {
+  try {
+    const order = await paypal.createOrder();
+    res.json(order);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// capture payment
+app.post("/api/orders/:orderID/capture", async (req, res) => {
+  const { orderID } = req.params;
+  try {
+    const captureData = await paypal.capturePayment(orderID);
+    res.json(captureData);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.listen(8888);
