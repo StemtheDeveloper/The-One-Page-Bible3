c6228deabba347e5868103044b6d671e96172655
StemtheDeveloper <127262716+StemtheDeveloper@users.noreply.github.com>
2023-11-28 22:14:56 +1300

    Did things

--- public/app.js
+++ public/app.js
@@ -82,37 +82,64 @@ if (paypal.HostedFields.isEligible()) {
           },
         })
         .then((payload) => {
-          console.log(`3DS-CONTINGENCY: liabilityShifted: ${payload.liabilityShifted} | authenticationStatus: ${payload.authenticationStatus} | authenticationReason: ${payload.authenticationReason} | Payload: ${JSON.stringify(payload)}`);
+          console.log(
+            `3DS-CONTINGENCY: liabilityShifted: ${
+              payload.liabilityShifted
+            } | authenticationStatus: ${
+              payload.authenticationStatus
+            } | authenticationReason: ${
+              payload.authenticationReason
+            } | Payload: ${JSON.stringify(payload)}`
+          );
           var redirectURL = "review?token=" + orderId;
-          var threeDSLiabilityShifted = '';
-          var threeDSAuthenticationStatus = '&3ds_auth_status=' + payload.authenticationStatus;
-          var threeDSAuthenticationReason = '&3ds_auth_reason=' + payload.authenticationReason;
-          redirectURL = redirectURL + threeDSAuthenticationStatus + threeDSAuthenticationReason;
+          var threeDSLiabilityShifted = "";
+          var threeDSAuthenticationStatus =
+            "&3ds_auth_status=" + payload.authenticationStatus;
+          var threeDSAuthenticationReason =
+            "&3ds_auth_reason=" + payload.authenticationReason;
+          redirectURL =
+            redirectURL +
+            threeDSAuthenticationStatus +
+            threeDSAuthenticationReason;
 
           if (payload.liabilityShifted) {
-              threeDSLiabilityShifted = '&3ds_liability_shifted=true';
-              redirectURL += threeDSLiabilityShifted;
+            threeDSLiabilityShifted = "&3ds_liability_shifted=true";
+            redirectURL += threeDSLiabilityShifted;
           }
           // No 3DS Contingency Triggered
           if (payload.liabilityShifted === undefined) {
-              threeDSLiabilityShifted = '&3ds_liability_shifted=undefined';
-              redirectURL += threeDSLiabilityShifted;
+            threeDSLiabilityShifted = "&3ds_liability_shifted=undefined";
+            redirectURL += threeDSLiabilityShifted;
           }
           // 3DS Contingency Triggered + Buyer skipped* 3DS
           if (payload.liabilityShifted === false) {
-              threeDSLiabilityShifted = '&3ds_liability_shifted=false';
-              redirectURL += threeDSLiabilityShifted;
+            threeDSLiabilityShifted = "&3ds_liability_shifted=false";
+            redirectURL += threeDSLiabilityShifted;
           }
-//        console.log(redirectURL);
-        redirect='http://localhost:8888/success';
-        href='https://www.sandbox.paypal.com/webapps/helios?action=verify&flow=3ds&cart_id='+orderId+'&redirect_uri='+redirect;
-//        document.getElementById('threeds').innerHTML="<Iframe src="+href+" />";
-          forthreeds = "<Dialog open> <a href='#' onclick=captureOrder(\'"+orderId+"\')>Close</a> <Iframe height=\"500\" width=\"500\" src="+href+" sandbox=\"allow-scripts allow-popups allow-same-origin\" referrerpolicy=\"origin\" /> </Dialog>";
-          document.getElementById('threeds').innerHTML=forthreeds;
+          //        console.log(redirectURL);
+          redirect = "http://localhost:8888/success";
+          href =
+            "https://www.sandbox.paypal.com/webapps/helios?action=verify&flow=3ds&cart_id=" +
+            orderId +
+            "&redirect_uri=" +
+            redirect;
+          //        document.getElementById('threeds').innerHTML="<Iframe src="+href+" />";
+          forthreeds =
+            "<Dialog open> <a href='#' onclick=captureOrder('" +
+            orderId +
+            '\')>Close</a> <Iframe height="500" width="500" src=' +
+            href +
+            ' sandbox="allow-scripts allow-popups allow-same-origin" referrerpolicy="origin" /> </Dialog>';
+          document.getElementById("threeds").innerHTML = forthreeds;
         })
         .catch((err) => {
           var msg = err.details[0].description;
-          alert("Payment could not be captured! \n Error - "+msg+" Debug ID - "+err.debug_id);
+          alert(
+            "Payment could not be captured! \n Error - " +
+              msg +
+              " Debug ID - " +
+              err.debug_id
+          );
         });
     });
   });
@@ -121,21 +148,20 @@ if (paypal.HostedFields.isEligible()) {
   document.querySelector("#card-form").style = "display: none";
 }
 function captureOrder(orderId) {
-document.getElementById('threeds').remove();
-fetch(`/api/orders/${orderId}/capture`, {
-             method: "post",
-           })
-             .then((res) => res.json())
-             .then((orderData) => {
-               const errorDetail =
-                 Array.isArray(orderData.details) && orderData.details[0];
-               if (errorDetail) {
-                 var msg = "Sorry, your transaction could not be processed.";
-                 if (errorDetail.description)
-                   msg += "\n\n" + errorDetail.description;
-                 if (orderData.debug_id) msg += " (" + orderData.debug_id + ")";
-                 return alert(msg); // Show a failure message
-               }
-               alert("Transaction completed! Transaction ID - "+orderData.id);
-             });
-             }
+  document.getElementById("threeds").remove();
+  fetch(`/api/orders/${orderId}/capture`, {
+    method: "post",
+  })
+    .then((res) => res.json())
+    .then((orderData) => {
+      const errorDetail =
+        Array.isArray(orderData.details) && orderData.details[0];
+      if (errorDetail) {
+        var msg = "Sorry, your transaction could not be processed.";
+        if (errorDetail.description) msg += "\n\n" + errorDetail.description;
+        if (orderData.debug_id) msg += " (" + orderData.debug_id + ")";
+        return alert(msg); // Show a failure message
+      }
+      alert("Transaction completed! Transaction ID - " + orderData.id);
+    });
+}
